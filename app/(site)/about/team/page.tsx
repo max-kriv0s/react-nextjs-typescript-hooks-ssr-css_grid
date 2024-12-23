d800bb13223fe61e22f3c4560a3e90f406a3be39
Maxim Krivoshein <max.kriv0s@gmail.com>
2024-12-23 19:37:40 +0300

    с 1 по 8 главы

---
+++ app/(site)/about/team/page.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Команда',
+};
+
+export default function Team() {
+  return <div>Команда</div>;
+}
---
+++ app/(site)/news/page.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Новости',
+};
+
+export default function News() {
+  return <div>Новости</div>;
+}
