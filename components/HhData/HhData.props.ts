f3c8473177f8af47be21cfed885c06467d3082f6
Maxim Krivoshein <max.kriv0s@gmail.com>
2025-01-09 09:38:24 +0300

    feat: add advantages, seoText and tags

---
+++ components/HhData/HhData.props.ts
@@ -0,0 +1,4 @@
+import { HhData } from '@/interfaces/page.interface';
+
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+export interface HhDataProps extends HhData {}
--- components/index.ts
+++ components/index.ts
@@ -3,6 +3,9 @@ export * from './Button/Button';
 export * from './P/P';
 export * from './Tag/Tag';
 export * from './Rating/Rating';
+export * from './Card/Card';
+export * from './HhData/HhData';
+export * from './Advantages/Advantages';
 
 export * from './Header/Header';
 export * from './Sidebar/Sidebar';
